d84d45799e90fc41c518ca80cad8590b75e624ad
Tatyana-JSc2 <tanya.tanechka912@gmail.com>
2023-12-03 19:56:34 +0300

    hw6 исправлено

--- scripts/hw6.js
+++ scripts/hw6.js
@@ -1,8 +1,11 @@
 const arr = [1, 5, 4, 10, 0, 3];
 
 for (let i = 0; i < arr.length; i++) {
-    if (arr[i] == 10) break;
+
     console.log(arr[i]);
+    if (arr[i] === 10) {
+        break;
+    }
 }
 
 const nums = [1, 5, 4, 10, 0, 3];
@@ -20,12 +23,15 @@ let numb = [1, 3, 5, 10, 20];
 numb = numb.join(' ');
 console.log(numb);
 
-let arrs = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
-for (let arrIn of arrs) {
-    for (let element of arrIn) {
-        console.log(element);
-    };
+let arrs = [];
+for (let i = 0; i < 3; i++) {
+    arrs[i] = [];
+    for (let j = 0; j < 3; j++) {
+        arrs[i][j] = 1;
+    }
 }
+console.log(arrs);
+
 
 let Numb = [1, 1, 1];
 Numb.push(2, 2, 2);
@@ -36,9 +42,9 @@ Arr = Arr.sort();
 let endArr = Arr.pop();
 console.log(Arr);
 
-/*let guessNum = [9, 8, 7, 6, 5];
+let guessNum = [9, 8, 7, 6, 5];
 let guess = Number(prompt('угадай число'));
-guessNum.filter(el => el === guess) > 0 ? alert('Угадал') : alert('Не угадал');*/
+guessNum.filter(el => el === guess) > 0 ? alert('Угадал') : alert('Не угадал');
 
 let GuessNum = [9, 8, 7, 6, 5];
 let Guess = Number(prompt('угадай число'));
@@ -50,48 +56,56 @@ arrString = arrString.reverse();
 arrString = arrString.join('');
 console.log(arrString);
 
+let String = 'abcdef';
+console.log(String.split('').reverse().join(''));
+
 let arrNum = [[1, 2, 3,], [4, 5, 6]];
-console.log(arrNum);
-arrNum = arrNum.join(',');
-console.log(arrNum);
-arrNum = arrNum.split();
-console.log(arrNum);
+console.log(arrNum.flat());
 
 const arr10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for (let i = 0; i < arr10.length; i++) {
-    if (arr10[i] == 10) break;
+for (let i = 0; i < arr10.length - 1; i++) {
     console.log(arr10[i] + arr10[i + 1]);
-};
+}
 
-const arrSquare = [2, 3, 4]
-let Square = arrSquare.map(item => item ** 2);
-console.log(Square);
+const arrSquare = [2, 3, 4];
+function Square(arrSquare) {
+    return arrSquare.map(item => item ** 2);
+}
+console.log(Square(arrSquare));
+
 
 const getWords = ['слово', '', 'слог', 'длинное предложение', 'буква'];
-let LengthWords = getWords.map(item => item.length);
-console.log(LengthWords);
+function LengthWords() {
+    return getWords.map(item => item.length);
+}
+console.log(LengthWords());
+
+const GetWords = ['слово', '', 'слог', 'длинное предложение', 'буква'];
+const lengthWords = GetWords.map(item => {
+    return item.length;
+});
+console.log(lengthWords);
 
 
-filterPositive1([-1, 0, 5, -10, 56]);
 function filterPositive1(array) {
+    let ArrPositive1 = [];
     for (let item of array) {
-        if (item < 0) console.log(item);
-    };
-};
+        if (item < 0) ArrPositive1.push(item);
+    }
+    return ArrPositive1;
+}
+console.log(filterPositive1([-1, 0, 5, -10, 56]));
 
-filterPositive2([-25, 25, 0, -1000, -2]);
+
+
 function filterPositive2(array) {
-    for (let item of array) {
-        if (item < 0) console.log(item);
-    };
-};
-
-
-/*const filterPositive = [-1, 0, 5, -10, 56];
-let Positive = filterPositive.filter(item=>item<0);
-console.log(Positive);*/
-
-
+    let ArrPositive2 = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < 0) ArrPositive2.push(array[i]);
+    }
+    return ArrPositive2;
+}
+console.log(filterPositive2([-25, 25, 0, -1000, -2]));
 
 
 
