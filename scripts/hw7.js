32c1d2c77230d605475f2e44cf73c2939af30c66
Tatyana-JSc2 <tanya.tanechka912@gmail.com>
2023-12-07 20:04:31 +0300

    hw7 задания 1,2

---
+++ scripts/hw7.js
@@ -0,0 +1,13 @@
+/*const string = 'js';
+console.log(string.toUpperCase());
+
+
+function searchStart(array, string) {
+    let result = [];
+    for (let item of array) {
+        if (item.toLowerCase().startsWith(string.toLowerCase())) result.push(item);
+    }
+    return result;
+}
+console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], "ко"));*/
+
